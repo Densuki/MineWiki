399f3f9859adcd0d34258e052749cb661c6a905f
Densuki <DarkShadow3315@gmail.com>
2020-04-13 18:13:56 -0300

    Update aspects.js

--- commands/Thaumcraft/aspects.js
+++ commands/Thaumcraft/aspects.js
@@ -9,6 +9,13 @@ module.exports = {
     description: "enviar um embed mostrando as combinações de aspectos",
 
     run: async (client, message, args) => {
+        
+                if (!message.member.roles.some(r => ["Linked"].includes(r.name)))
+          return message
+            .reply(
+              "Você não tem as permissões necessárias para usar este comando."
+            )
+            .then(m => m.delete(5000));
 
         {//EMOJI
          var logo = "https://imgur.com/pgQnTtD.png";
