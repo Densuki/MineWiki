0a8e89a71ee8d9d3797569334c577271c23c1759
Densuki <DarkShadow3315@gmail.com>
2020-04-13 18:15:42 -0300

    Update minecraft.js

--- commands/Minecraft/minecraft.js
+++ commands/Minecraft/minecraft.js
@@ -8,6 +8,13 @@ module.exports = {
     description: "enviar um embed mostrando as opções",
 
     run: async (client, message, args) => {
+        
+        if (!message.member.roles.some(r => ["Linked"].includes(r.name)))
+          return message
+            .reply(
+              "Você não tem as permissões necessárias para usar este comando."
+            )
+            .then(m => m.delete(5000));
 
 
     }
