9d856c5275e2a14023d0696222be73e8129155b5
Densuki <DarkShadow3315@gmail.com>
2020-04-13 18:23:39 -0300

    Update prune.js

--- commands/moderation/prune.js
+++ commands/moderation/prune.js
@@ -9,7 +9,7 @@ module.exports = {
     run: (client, message, args) => {
 	message.delete();		
   //if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Você não pode fazer isso! Você não possui permissões para executar esse comando!");
-  if (!message.member.roles.some(r=>["Linked", "STAFF", "Staff"].includes(r.name))) return message.reply("Você não pode fazer isso! Você não possui permissões para executar esse comando!");
+  if (!message.member.roles.some(r=>["Linked"].includes(r.name))) return message.reply("Você não pode fazer isso! Você não possui permissões para executar esse comando!");
   if(!args[0]) return message.channel.send("Nain!\n Esqueceu de adicionar um número!").then(msg => msg.delete(10000));
   message.channel.bulkDelete(args[0]).then(() => {
   message.channel.send(`Mensagens ${args[0]} foram apagadas.`).then(msg => msg.delete(300));});
